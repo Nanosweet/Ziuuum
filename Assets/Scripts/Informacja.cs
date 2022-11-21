5cd49a5b83e4249981f9eac775f738fbb5d58117
unknown <nanosweet321@gmail.com>
2022-11-21 22:06:20 +0100

    base

---
+++ Assets/Scripts/Informacja.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Informacja : MonoBehaviour
+{
+
+    Text info;
+    // Start is called before the first frame update
+    void Start()
+    {
+        info = GetComponent<Text>();
+        info.text = "asddddddddddddddddddd";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
+    private float i = 0;
+    [SerializeField] private float force = 0;
+
+    private Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Naciskasz spacje force sie inkrementuje
+
+        //Input.GetKey
         
+        if(Input.GetKey(KeyCode.Space))
+        {
+            force++;            
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * force * 5 * Time.deltaTime, ForceMode.Impulse);
+            Debug.Log("Klikles spacje " + force);
+        }
+        if(Input.GetKeyUp(KeyCode.G))
+        {
+            transform.position = startPos;
+            rb.AddForce(0,0,0);
+        }
     }
 }
