39f7966c6b93963580930a294419d4e2fe8f60dd
Nanosweet <nanosweet321@gmail.com>
2022-12-05 14:56:41 +0100

    linerenderer shader + change direction line

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,10 +10,16 @@ public class Player : MonoBehaviour
 
     [SerializeField] Text currentTime;
 
+
+    float _lineZPos = -1f;
+    float _lineYPos = 0.3f;
+
     bool stoi = false;
     public float stopVelocity;
 
     public GameObject mCamera;
+
+    [SerializeField] Transform playerPos;
 
 
     Color c1 = Color.black;
@@ -21,7 +27,7 @@ public class Player : MonoBehaviour
 
 
 
-    // LINERENDERER
+    // Linerenderer
     [SerializeField] LineRenderer aimLine, dirLine;
 
     // Start is called before the first frame update
@@ -36,23 +42,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        var mousePos = ClickedPoint(); // get MousePos
+        Vector3 dir2 = rb.position - mousePos;
+
+
+        // Aim Linerenderer index 0 coordinates position
+        Vector3 aimLineIndex0 = new Vector3(rb.transform.position.x, _lineYPos, rb.transform.position.z);
+        Vector3 aimLineIndex1 = new Vector3(mousePos.x, _lineYPos, mousePos.z);
+        Vector3 aimLineIndex1v2 = new Vector3(dir2.x, _lineYPos, dir2.z);
+        aimLine.SetPosition(0, aimLineIndex0);
+
+        
+
         if(Input.GetKey(KeyCode.Mouse0))
        {
-           aimLine.SetPosition(1, ClickedPoint());
+           aimLine.SetPosition(1, -mousePos);
        }
         //aimLine.SetPosition(1, ClickedPoint());
 
         //dirLine.SetColors(c1,c2);
 
         //dirLine.SetPosition(0, rb.transform.position );
+        
 
-        var x = ClickedPoint();
-        Vector3 dir2 = GetComponent<Rigidbody>().position - x;
+        //aimLine.SetPosition(0, new Vector3(rb.transform.position.x, _lineYPos, rb.transform.position.z));
+        aimLine.SetPosition(1, dir2);
+
+        
 
 
         //dirLine.SetPosition(1, dir2);
 
-        Debug.Log(rb.velocity.magnitude );
+        Debug.Log("rb.velo:"+rb.velocity.magnitude);
     }
 
     private void FixedUpdate()
@@ -69,7 +91,7 @@ public class Player : MonoBehaviour
         
 
         
-        stoi = true;
+       // stoi = true;
     }
 
 
@@ -77,12 +99,9 @@ public class Player : MonoBehaviour
     Vector3 ClickedPoint()
     {
         Vector3 position = Vector3.zero; // zmienna position = 0,0,0
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // castowanie ray od kamery do pozycji myczy na ekranie
-        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // castowanie ray od kamery do pozycji myczy na ekranie        
 
-        RaycastHit hit = new RaycastHit();
-
-        
+        RaycastHit hit = new RaycastHit();        
 
         if(Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
@@ -115,7 +134,7 @@ public class Player : MonoBehaviour
         
 
         Vector3 dir = GetComponent<Rigidbody>().position - endPos;
-        GetComponent<Rigidbody>().AddForce(dir * force, ForceMode.Impulse);
+        //GetComponent<Rigidbody>().AddForce(dir * force, ForceMode.Impulse);
 
         //Debug.Log("Pierdolniecie: "+ rb.velocity );
     }
